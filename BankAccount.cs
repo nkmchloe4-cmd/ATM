ff13d1bdc172e822d280112ba0583a0bcbdab109
Chloe Nakamanya <nakamanyachloe4@gmail.com>
2025-09-21 18:05:55 +0200

    "Add BankAccount class"

---
+++ BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    internal class BankAccount
+    {
+        private decimal balance;
+
+        public decimal Balance
+        {
+            get { return balance; }
+        }
+
+        public void Deposit(decimal amount)
+        {
+            {
+                if (amount <= 0) balance += amount;
+            }
+
+
+        }
+   public void Withdraw(decimal amount)
+        {
+            if (amount > 0 && amount <= balance)
+            
+                balance -= amount;            
+        }
+
+    }
+}
